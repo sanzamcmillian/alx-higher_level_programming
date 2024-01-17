dfd26f02af6fd26024c7dc6209082bd38bc412c4
sanzamcmillian <g18s2658@ru.ac.za>
2024-01-17 09:32:54 +0200

    json1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+import json
+import csv
+
 class Base:
     __nb_objects = 0
     def __init__(self, id=None):
@@ -7,3 +11,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+           return ("[]")
+        return json.dumps(list_dictionaries)
+
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -48,3 +48,6 @@ class Square(Rectangle):
                      self.x = v
                 elif k == "y":
                      self.y = v
+
+    def to_dictionary(self):
+        return({"id": self.id, "size": self.width, "x": self.x, "y": self.y})
