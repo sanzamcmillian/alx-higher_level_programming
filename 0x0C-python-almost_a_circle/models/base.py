8cbb8a6fb5e4f8a067f9449e48423569801478e8
sanzamcmillian <g18s2658@ru.ac.za>
2024-01-17 09:56:47 +0200

    json3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,18 @@ class Base:
            return ("[]")
         return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+               jsonfile.write("[]")
+            else:
+               list_dicts = [o.to_dictionary() for o in list_objs]
+               jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "[]":
+           return ([])
+        return (json.loads(json_string))
