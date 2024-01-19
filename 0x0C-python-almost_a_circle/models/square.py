78be67816e84b5b2dd4a97a28ab96d110f5e5bc8
sanzamcmillian <g18s2658@ru.ac.za>
2024-01-19 12:24:53 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,22 +3,61 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """
+    Square class represents a geometric square and inherits from the Rectangle class.
+
+    Attributes:
+        size (int): The size of the square.
+        x (int): The x-coordinate of the top-left corner of the square.
+        y (int): The y-coordinate of the top-left corner of the square.
+        id (int): The identifier of the square.
+    """
     def __init__(self, size, x = 0, y = 0, id = None):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int): The size of the square.
+            x (int): The x-coordinate of the top-left corner of the square.
+            y (int): The y-coordinate of the top-left corner of the square.
+            id (int): The identifier of the square.
+        """
         super().__init__(size, size, x, y, id)
         
     @property
     def size(self):
+        """
+        Getter method for the size property.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        Setter method for the size property.
+
+        Parameters:
+            value (int): The new size of the square.
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
+         """
+        Returns a string representation of the square.
+
+        Returns:
+            str: A string representation of the square.
+        """
         return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size))
 
     def update(self, *args, **kwargs):
+        """
+        Updates the attributes of the square using either positional arguments or keyword arguments.
+        """
         if args and len(args) != 0:
            a = 0 
            for arg in args:
@@ -50,4 +89,10 @@ class Square(Rectangle):
                      self.y = v
 
     def to_dictionary(self):
+        """
+        Converts the square object to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the square.
+        """
         return({"id": self.id, "size": self.width, "x": self.x, "y": self.y})
