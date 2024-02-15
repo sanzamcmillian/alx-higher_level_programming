fa118fed51ea95e7c9b5d2d330238478ca00ea61
sanzamcmillian <g18s2658@ru.ac.za>
2024-02-15 12:57:06 +0200

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Creates table called first_table with values id and name in My SQL server.
-CREATE table IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE table IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(256));
