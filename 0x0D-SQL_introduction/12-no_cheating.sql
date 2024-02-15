1cbffa2f5edf1e5d5581c5bf09127d56c625e8ff
sanzamcmillian <g18s2658@ru.ac.za>
2024-02-15 13:19:48 +0200

    5

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
 -- Updates the score in the table second_table for Bob to 10 in my MySQL server.
 UPDATE second_table
-SET 'score' = 10
-WHERE 'name' = "Bob";
+SET score = 10
+WHERE name = "Bob";
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Lists all the records of the table second_table having a name value in my MySQL server.
 -- Records ordered by descending score
-SELECT 'score', 'name'
+SELECT score, name
 FROM second_table
-WHERE 'name' != ""
-ORDER BY 'score' DESC;
+WHERE name IS NOT NULL
+ORDER BY score DESC;
