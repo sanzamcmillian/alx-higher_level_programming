22b34a8ca6ebc9c05d8d74bac1a35e34cceba5ff
sanzamcmillian <g18s2658@ru.ac.za>
2024-02-15 14:10:19 +0200

    101

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- Converts the entire database hbtn_0c_0 to UTFS
 ALTER TABLE hbtn_oc_c CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4_unicode_ci;
+
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
