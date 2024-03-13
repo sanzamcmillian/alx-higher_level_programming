b614039d1d0a0be4a8ca44980cfaee097dc8597a
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-13 10:47:00 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const [,, arg] = process.argv
-const num = parseInt(arg)
+const [,, arg] = process.argv;
+const num = parseInt(arg);
 
 if (!isNaN(num)) {
   console.log('My number: ' + num);
