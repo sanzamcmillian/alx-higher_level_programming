e3a72248c95c45b5fc5d32f3b84b4b28865ab313
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-17 09:07:22 +0200

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    db =  MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE LIBRARY '{}'"
@@ -14,4 +14,5 @@ if __name__ == "__main__":
     for row in rows:
         print(rows)
     cur.close()
-    db.close()    
+    db.close()
+      
