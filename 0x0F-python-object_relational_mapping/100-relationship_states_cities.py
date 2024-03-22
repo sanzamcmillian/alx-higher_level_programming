0da2aaffef382519bf3165ef2d08237ff93b19ea
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-22 16:35:24 +0200

    pyc100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,14 +12,11 @@ if __name__ == '__main__':
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
-    
     newState = State(name='California')
     newCity = City(name='San Francisco')
     newState.cities.append(newCity)
-    
     session.add(newState)
     session.add(newCity)
     session.commit()
