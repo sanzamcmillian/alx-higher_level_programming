ae3e75b57e64a1b9fe7dae1f24e3de167f60aa7a
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-22 10:26:50 +0200

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities JOIN states ON cities.state_id ORDER BY cities.id ASC""")
+                cities INNER JOIN states ON states.id=cities.id""")
     rows = cur.fetchball()
     for row in rows:
         print(row)
