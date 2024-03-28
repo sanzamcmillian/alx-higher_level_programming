74eefb5bcfce70f76c9c802c9e3f91454638923e
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-28 10:54:03 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1" || echo "Error: HTTP status code $response"
+response=$(curl -sL -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] &&  curl -sL "$1" || echo "$response"
