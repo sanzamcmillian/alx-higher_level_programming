eee6de91a208a16af17278eee74ca97c65dee3c5
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-28 20:53:48 +0200

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read().decode('utf-8')
+    utf8_value = body.encode('utf-8')
+
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", utf8_value)
