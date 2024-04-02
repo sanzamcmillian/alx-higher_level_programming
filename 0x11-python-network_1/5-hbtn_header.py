0b259edf77d060f2e19e7633543b2059b9a814c0
Sanele Skhosana <36506473+sanzamcmillian@users.noreply.github.com>
2024-04-02 04:38:00 +0200

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,6 +9,6 @@ response = requests.get(url)
 
 if 'X-Request-Id' in response.headers:
     x_request_id = response.headers['X-Request-Id']
-    print("X-Request-Id:", x_request_id)
+    print(x_request_id)
 else:
     print("X-Request-Id header not found in the response.")
