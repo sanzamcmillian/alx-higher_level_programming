333a23899983301294230facb6ca3a4a090dca6b
sanzamcmillian <g18s2658@ru.ac.za>
2024-04-02 04:10:32 +0200

    Create 5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL """
+import requests
+import sys
+
+url = sys.argv[1]
+
+response = requests.get(url)
+
+if 'X-Request-Id' in response.headers:
+    x_request_id = response.headers['X-Request-Id']
+    print("X-Request-Id:", x_request_id)
+else:
+    print("X-Request-Id header not found in the response.")
+
