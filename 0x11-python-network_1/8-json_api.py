88a2c2e499b1f097c742c0a9f039979551ce50f7
sanzamcmillian <g18s2658@ru.ac.za>
2024-04-02 04:20:04 +0200

    Create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter. """
+import requests
+import sys
+
+# Get the letter from command line arguments or set to an empty string if no argument is given
+q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+# URL for the POST request
+url = "http://0.0.0.0:5000/search_user"
+
+# Payload for the POST request
+payload = {'q': q}
+
+# Send POST request
+response = requests.post(url, data=payload)
+
+# Check if response contains valid JSON
+try:
+    data = response.json()
+except ValueError:
+    print("Not a valid JSON")
+    sys.exit()
+
+# Check if JSON is empty
+if not data:
+    print("No result")
+else:
+    print("[{}] {}".format(data['id'], data['name']))
+
