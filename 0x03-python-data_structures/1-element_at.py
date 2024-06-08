f24bb44768ce9846bdff642f2f570ff7f5e08a1f
sanzamcmillian <g18s2658@ru.ac.za>
2024-06-08 16:13:27 +0200

    up1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx >= len(my_list):
-        return None
-    return my_list[idx]
 
+def element_at(my_list, idx):
+    if idx > len(my_list)-1 or idx < 0:
+        return None
+    else:
+        return my_list[idx]
