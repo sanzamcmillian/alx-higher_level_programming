4cae6b227f7ed7b054e957d0d5fc24bdf946edbc
sanzamcmillian <g18s2658@ru.ac.za>
2024-06-08 16:17:24 +0200

    up4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
@@ -7,4 +8,3 @@ def new_in_list(my_list, idx, element):
     new_list = list(my_list)
     new_list[idx] = element
     return new_list
-
